0224160cff4bf0ed277c247f0afa3e46c18995ac
bee256 <bernd@engist.net>
2024-12-20 18:23:52 +0100

    Changes build_macOS.sh now includes statement to add data in data_no_git.

--- scripts/build_macOS.sh
+++ scripts/build_macOS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 source .venv/bin/activate
 python scripts/update_version.py
-pyinstaller --onedir --name StarDodge --windowed --add-data "assets:assets" -i ./assets/images/space_ship3.icns game.py
+pyinstaller --onedir --name StarDodge --windowed --add-data "assets:assets" --add-data "data_no_git:data_no_git" -i ./assets/images/space_ship3.icns game.py
